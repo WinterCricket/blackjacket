24a10e2b997093d95bb928f081832cecd47d848e
wintercricket <hodhodhod2003@gmail.com>
2021-06-30 20:58:11 -0500

    stage at a time got diff functions to work, except generate next hand

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 
 
+
+
 let firstCard = Math.round(Math.random()*9 +2);
 let secondCard = Math.round(Math.random()*9 +2);
 
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard;
 
-console.log(sum); 
+
+let sumEl = document.getElementById("sum-el");
+
+let saveEl = document.getElementById("save-el");
+
+function deal(){
+	sumEl.textContent = sum;
+}
+
+
+
+function save() {
+	let sumStr = sum + "    ";
+	saveEl.textContent += sumStr; 
+	sum = 0;
+	sumEl.textContent = sum;
+ }

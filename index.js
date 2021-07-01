2779ce74382b78c781fde986e31a9e861d5ef6b7
wintercricket <hodhodhod2003@gmail.com>
2021-06-30 22:37:03 -0500

    added the conditionals to the deal function for message output

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 let sumEl = document.getElementById("sum-el");
 
 let saveEl = document.getElementById("save-el");
+let messageEl = document.getElementById("message-el");
 
 function deal(){
 	let firstCard = Math.round(Math.random()*9 +2);
 	let secondCard = Math.round(Math.random()*9 +2);
 	let newSum = firstCard + secondCard;
 	sumEl.textContent = newSum;
+
+	if(newSum < 21){
+		messageEl.textContent = "Want another card?";
+	} else if(newSum == 21) {
+		messageEl.textContent = "TWENTY-ONE!";
+	} else {
+		messageEl.textContent = "You busted!";
+	}
 }
 
 
@@ -17,7 +26,11 @@ function save() {
 	let sumStr = sumEl.textContent + "  ";
 	saveEl.textContent += sumStr; 
 
-	 sum = "Hit me!";
-	 sumEl.textContent = sum;
+	sum = "New deal!";
+	sumEl.textContent = sum;
 	console.log(sumStr);
 }
+
+
+//conditionals
+

f47ca65b9921d3bd2b02c3b2bf676b53632f3e41
wintercricket <hodhodhod2003@gmail.com>
2021-07-10 20:29:47 -0500

    try out js logic and other delusions to get game to play itself

--- index.js
+++ index.js
@@ -8,79 +8,92 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-function deal(){
-	let firstCard = Math.round(Math.random()*9 +2);
-	let secondCard = Math.round(Math.random()*9 +2);
-	let thirdCard = Math.round(Math.random()*9 +2);
-	let newSum = firstCard + secondCard + thirdCard;
-	sumEl.textContent = newSum;
-/* 
-	pseudocode
+// function deal(){
+// 	let firstCard = Math.round(Math.random()*9 +2);
+// 	let secondCard = Math.round(Math.random()*9 +2);
+// 	let thirdCard = Math.round(Math.random()*9 +2);
+// 	let newSum = firstCard + secondCard + thirdCard;
+ 	//sumEl.textContent = newSum;
+ 
+	
 
-	let card = Math.round(math.random()*9+2);
-
-	or
+	
 function deal(){
+
 	let firstCard = Math.round(Math.random()*9 +2);
 	let secondCard = Math.round(Math.random()*9 +2);
 	let thirdCard = Math.round(Math.random()*9 +2);
 	let fourthCard = Math.round(Math.random()*9 +2);
 	let fifthCard = Math.round(Math.random()*9 +2);
 	let sum = firstCard + secondCard;
-	let sum2 = sum + thirdCard;
-	let sum3 = sum2 + fourthCard;
-	let sum4 = sum3 + fifthCard;
-
-
-	if(sum <= 20){
-		message = "Want another card?";
+	
+	if(sum <= 17){
+		let sumA = sum + thirdCard;
+		
+		sumEl.textContent = sumA;
+		
+		if(sumA <= 17 ) {
+			let sumB = sumA + fourthCard;
+			sumEl.textContent = sumB;
+			if(sumB <= 17){
+				let sumC = sumB + fifthCard;
+				sumEl.textContent = sumC;
+				messageEl.textContent = "You won with five cards!";
+			} else {
+				messageEl.textContent = "You went over! You lose.";
+			}
+		} else if (sumA > 20 || sumA < 22){
+			return message = "Blackjack. Hold and win!";
+			messageEl.textContent = message;
+		}
+		
+	} else if (sum === 21){
+		return message = "Blackjack. Hold and win!";
 		messageEl.textContent = message;
-	}
-
-	
-
-
-
-	function dealCard () {
-		let result = sum2;
-	}
-	
-deal another card
-
-	if(){
-	
-	}
-
-
-
-
-	
-*/
-
-
-
-	if(newSum <= 20){
-		message = "Want another card?";
-		messageEl.textContent = message;
-	} else if(newSum === 21) {
-		message = "TWENTY-ONE! You may win!";
-		messageEl.textContent = message;
-		// hasBlackJack = true;
 	} else {
-		message = "You have busted! You're out of the game.";
+		return message = "You broke your chances! You went over TWENTY-ONE.";
 		messageEl.textContent = message;
-		// isAlive = false;
 	}
-}
 
-
-
-function save() {
-	let sumStr = sumEl.textContent + "  ";
-	saveEl.textContent += sumStr; 
-
-	sum = "New deal!";
-	sumEl.textContent = sum;
 	
+
+
+	
+
+	
+
+
+
+	
+
+
+
+	
+
+
+
+
+	// if(newSum <= 20){
+	// 	message = "Want another card?";
+	// 	messageEl.textContent = message;
+	// } else if(newSum === 21) {
+	// 	message = "TWENTY-ONE! You may win!";
+	// 	messageEl.textContent = message;
+	// 	// hasBlackJack = true;
+	// } else {
+	// 	message = "You have busted! You're out of the game.";
+	// 	messageEl.textContent = message;
+	// 	// isAlive = false;
+	// }
+
 }
+
+
+// function save() {
+// 	let sumStr = sumEl.textContent + "  ";
+// 	saveEl.textContent += sumStr; 
+
+// 	sumEl.textContent = sum;
+	
+// }
 

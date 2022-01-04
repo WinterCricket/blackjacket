271611fa72cbf140818bdcb5b6415ba1e4dc66cd
wintercricket <hodhodhod2003@gmail.com>
2022-01-04 01:31:20 -0600

    add weird changes see what happens

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
+querySelector;
 
 let player = {
-	name: "Roddy",
-	chips: 140
-}
+  name: "Roddy",
+  chips: 140,
+};
 
 let saveEl = document.getElementById("save-el");
 let messageEl = document.getElementById("message-el");
@@ -12,86 +12,70 @@ let isAlive = false;
 let message = "";
 let sum = 0;
 
-
-
 let cards = [];
-let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("sum-el");
 
-let cardsEl = document.getElementById("cards-el");
+let cardsEl = document.querySelector("cards-el");
 
 //oop
 
+let playerEl = document.querySelector("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
-let playerEl = document.getElementById("player-el");
-playerEl.textContent = player.name +": $"+player.chips ;
-
-function getRandomCard(){
-	
-	let randomNumber = Math.floor(Math.random()*13) +1;
-	if(randomNumber > 10){
-		return 10;
-	} else if (randomNumber === 1){
-		return 11;
-	} else {
-		return randomNumber;
-	}
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
-function renderGame(){
-	cardsEl.textContent = "Cards: " ;
-	for (let i = 0; i < cards.length; i++){
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+  sumEl.textContent = "Sum: " + sum;
 
-		cardsEl.textContent +=  cards[i] + " ";
-	}
-	sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Want another card?";
+    isAlive = true;
+  } else if (sum === 21) {
+    message = "BlackJacket!";
+    hasBlackJack = true;
+  } else {
+    message = "You went over. You're out of the game.";
 
-	if(sum <= 20){
-		message = "Want another card?";
-		isAlive = true;
-	}
+    isAlive = false;
+  }
 
-	else if(sum === 21) {
-		message = "BlackJacket!";
-		hasBlackJack = true;
-	} else {
-		message = "You went over. You're out of the game.";
-
-		isAlive = false;
-
-	}
-
-	messageEl.textContent = message;
-	//sumEl.textContent = "Sum: "+sum;
+  messageEl.textContent = message;
+  //sumEl.textContent = "Sum: "+sum;
 }
 
-function newCard(){
-	if(isAlive && hasBlackJack === false){
+function newCard() {
+  if (isAlive && hasBlackJack === false) {
+    let card = getRandomCard();
 
-	let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+  }
 
-	sum += card;
-	cards.push(card);
-} 
-
-	renderGame();
+  renderGame();
 }
 
+function startGame() {
+  let isAlive = true;
 
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
 
+  cards = [firstCard, secondCard];
 
-function startGame(){
+  sum = firstCard + secondCard;
 
-	let isAlive = true;
-
-	let firstCard = getRandomCard();
-	let secondCard = getRandomCard();
-
-	cards = [firstCard, secondCard];
-
-	sum = firstCard + secondCard;
-	
-	renderGame();
-	
+  renderGame();
 }
-
-
